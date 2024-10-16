66d91ba48b05a20024c15293c5079af2dadf2d82
Nemessi10 <hgjghcbx@gmail.com>
2024-10-16 18:05:06 +0300

    added several templates

--- app.py
+++ app.py
@@ -13,20 +13,34 @@ def home():
 
     agent = request.user_agent
 
-    return render_template("home.html", agent = agent)
+    return render_template("home.html", agent=agent)
 
 @app.route("/hi/<string:name>") #/hi/roman?age=19
 def greetings(name):
     name = name.upper()
     age = request.args.get("age", 0, int)
-    year = 2024 - age
 
-    return f"Welcome, {name}, {year}"
+    return render_template("hi.html", name=name, age=age)
 
 @app.route("/admin")
 def admin():
     to_url = url_for("greetings", name = "administrotor", age = 19)
     return redirect(to_url)
+
+posts = [
+    {"id": 0, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 1, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 2, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+@app.route("/posts")
+def get_posts():
+    return render_template("posts.html", posts=posts)
+
+@app.route("/post/<int:id>")
+def detail_post(id):
+    post = posts[id]
+    return render_template("detail_post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
